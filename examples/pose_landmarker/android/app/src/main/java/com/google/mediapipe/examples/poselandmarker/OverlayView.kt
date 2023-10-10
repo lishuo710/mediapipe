86e3c40c1c869c8a058acf3cb0081a155fced62a
lishuo <lishuo@juze.pro>
2023-10-10 14:36:11 +0800

    feat: compute node angle and draw arc

--- examples/pose_landmarker/android/app/src/main/java/com/google/mediapipe/examples/poselandmarker/OverlayView.kt
+++ examples/pose_landmarker/android/app/src/main/java/com/google/mediapipe/examples/poselandmarker/OverlayView.kt
@@ -19,14 +19,17 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
-import androidx.core.content.ContextCompat
+import com.google.mediapipe.tasks.components.containers.NormalizedLandmark
 import com.google.mediapipe.tasks.vision.core.RunningMode
-import com.google.mediapipe.tasks.vision.poselandmarker.PoseLandmarker
 import com.google.mediapipe.tasks.vision.poselandmarker.PoseLandmarkerResult
+import kotlin.math.atan2
 import kotlin.math.max
 import kotlin.math.min
+import kotlin.math.round
 
 class OverlayView(context: Context?, attrs: AttributeSet?) :
     View(context, attrs) {
@@ -52,38 +55,226 @@ class OverlayView(context: Context?, attrs: AttributeSet?) :
     }
 
     private fun initPaints() {
-        linePaint.color =
-            ContextCompat.getColor(context!!, R.color.mp_color_primary)
+        linePaint.color = Color.YELLOW
         linePaint.strokeWidth = LANDMARK_STROKE_WIDTH
-        linePaint.style = Paint.Style.STROKE
+        linePaint.style = Paint.Style.FILL_AND_STROKE
 
-        pointPaint.color = Color.YELLOW
-        pointPaint.strokeWidth = LANDMARK_STROKE_WIDTH
-        pointPaint.style = Paint.Style.FILL
+        pointPaint.color = Color.RED
+        pointPaint.strokeWidth = LANDMARK_POINT_WIDTH
+        pointPaint.style = Paint.Style.FILL_AND_STROKE
     }
 
     override fun draw(canvas: Canvas) {
         super.draw(canvas)
         results?.let { poseLandmarkerResult ->
             for(landmark in poseLandmarkerResult.landmarks()) {
-                for(normalizedLandmark in landmark) {
-                    canvas.drawPoint(
+                var postLandmark = landmark
+                var left = landmark[25]
+                var right = landmark[26]
+
+                var isLeft = left.z() < 0 && right.z() > 0
+                var isRight = left.z() > 0 && right.z() < 0
+                var isfull = left.z() * right.z() > 0
+
+                if(isLeft)
+                {
+                    postLandmark = landmark.filterIndexed { index, _ -> index in listOf(27,25,23,11,13,15) }
+                    renderLeft(canvas,landmark)
+                }
+
+                if(isRight)
+                {
+                    postLandmark = landmark.filterIndexed { index, _ -> index in listOf(28,26,24,12,14,16) }
+                    renderRight(canvas,landmark)
+                }
+
+                if(isfull)
+                {
+                    postLandmark = landmark.filterIndexed { index, _ -> index in listOf(27,25,23,11,13,15,28,26,24,12,14,16) }
+                    renderFull(canvas,landmark)
+                }
+
+
+                for(normalizedLandmark in postLandmark) {
+                    canvas.drawCircle(
                         normalizedLandmark.x() * imageWidth * scaleFactor,
                         normalizedLandmark.y() * imageHeight * scaleFactor,
+                        3F,
                         pointPaint
                     )
                 }
-
-                PoseLandmarker.POSE_LANDMARKS.forEach {
-                    canvas.drawLine(
-                        poseLandmarkerResult.landmarks().get(0).get(it!!.start()).x() * imageWidth * scaleFactor,
-                        poseLandmarkerResult.landmarks().get(0).get(it.start()).y() * imageHeight * scaleFactor,
-                        poseLandmarkerResult.landmarks().get(0).get(it.end()).x() * imageWidth * scaleFactor,
-                        poseLandmarkerResult.landmarks().get(0).get(it.end()).y() * imageHeight * scaleFactor,
-                        linePaint)
-                }
             }
         }
+    }
+
+    private fun renderLeft (canvas:Canvas,landmark: List<NormalizedLandmark>) {
+        canvas.drawLine(
+            landmark[27].x() * imageWidth * scaleFactor,
+            landmark[27].y() * imageHeight * scaleFactor,
+            landmark[25].x() * imageWidth * scaleFactor,
+            landmark[25].y() * imageHeight * scaleFactor,
+            linePaint
+        )
+
+        canvas.drawLine(
+            landmark[25].x() * imageWidth * scaleFactor,
+            landmark[25].y() * imageHeight * scaleFactor,
+            landmark[23].x() * imageWidth * scaleFactor,
+            landmark[23].y() * imageHeight * scaleFactor,
+            linePaint
+        )
+
+        canvas.drawLine(
+            landmark[23].x() * imageWidth * scaleFactor,
+            landmark[23].y() * imageHeight * scaleFactor,
+            landmark[11].x() * imageWidth * scaleFactor,
+            landmark[11].y() * imageHeight * scaleFactor,
+            linePaint
+        )
+
+        canvas.drawLine(
+            landmark[11].x() * imageWidth * scaleFactor,
+            landmark[11].y() * imageHeight * scaleFactor,
+            landmark[13].x() * imageWidth * scaleFactor,
+            landmark[13].y() * imageHeight * scaleFactor,
+            linePaint
+        )
+
+        canvas.drawLine(
+            landmark[13].x() * imageWidth * scaleFactor,
+            landmark[13].y() * imageHeight * scaleFactor,
+            landmark[15].x() * imageWidth * scaleFactor,
+            landmark[15].y() * imageHeight * scaleFactor,
+            linePaint
+        )
+
+        renderArc(canvas,Pair(landmark[23].x() * imageWidth * scaleFactor, landmark[23].y() * imageHeight * scaleFactor),Pair(landmark[25].x() * imageWidth * scaleFactor, landmark[25].y() * imageHeight * scaleFactor)
+            ,Pair(landmark[27].x() * imageWidth * scaleFactor, landmark[27].y() * imageHeight * scaleFactor))
+
+        renderArc(canvas,Pair(landmark[25].x() * imageWidth * scaleFactor, landmark[25].y() * imageHeight * scaleFactor),Pair(landmark[23].x() * imageWidth * scaleFactor, landmark[23].y() * imageHeight * scaleFactor)
+            ,Pair(landmark[11].x() * imageWidth * scaleFactor, landmark[11].y() * imageHeight * scaleFactor))
+
+        renderArc(canvas,Pair(landmark[23].x() * imageWidth * scaleFactor, landmark[23].y() * imageHeight * scaleFactor),Pair(landmark[11].x() * imageWidth * scaleFactor, landmark[11].y() * imageHeight * scaleFactor)
+            ,Pair(landmark[13].x() * imageWidth * scaleFactor, landmark[13].y() * imageHeight * scaleFactor))
+
+        renderArc(canvas,Pair(landmark[15].x() * imageWidth * scaleFactor, landmark[15].y() * imageHeight * scaleFactor),Pair(landmark[13].x() * imageWidth * scaleFactor, landmark[13].y() * imageHeight * scaleFactor)
+            ,Pair(landmark[11].x() * imageWidth * scaleFactor, landmark[11].y() * imageHeight * scaleFactor))
+    }
+
+    private fun renderArc(canvas:Canvas,startLine: Pair<Float, Float>, intersection: Pair<Float, Float>, endLine: Pair<Float, Float>) {
+        val paint = Paint().apply {
+            color = Color.GREEN
+            style = Paint.Style.STROKE
+            strokeWidth = 8f
+        }
+
+        val centerX = intersection.first
+        val centerY = intersection.second
+        val radius = 12F
+
+        val startAngle = calculateAngle(startLine.first, startLine.second, centerX, centerY) // 圆弧的起始角度
+        val endAngle = calculateAngle(endLine.first, endLine.second, centerX, centerY) // 圆弧的结束角度
+        val rect = RectF(centerX - radius, centerY - radius, centerX + radius, centerY + radius)
+
+        val path = Path()
+        var sweepAngle = endAngle - startAngle
+        if(sweepAngle < 0 )
+        {
+            sweepAngle += 360f;
+        }
+        path.arcTo(rect, startAngle, sweepAngle, true)
+
+        canvas.drawPath(path, paint)
+
+        val textPaint = Paint().apply {
+            color = Color.WHITE
+            textSize = 18f
+            textAlign = Paint.Align.LEFT
+        }
+        canvas.drawText("${round(sweepAngle)}",centerX+3f,centerY+18f,textPaint)
+    }
+
+    private  fun calculateAngle(x1: Float, y1: Float, centerX: Float, centerY: Float): Float {
+        val deltaX = x1 - centerX
+        val deltaY = y1 - centerY
+        val radians = atan2(deltaY.toDouble(), deltaX.toDouble())
+        val degrees = Math.toDegrees(radians.toDouble()).toFloat()
+        return if (degrees < 0) degrees + 360f else degrees
+    }
+
+    private fun renderRight (canvas:Canvas,landmark: List<NormalizedLandmark>) {
+        canvas.drawLine(
+            landmark[28].x() * imageWidth * scaleFactor,
+            landmark[28].y() * imageHeight * scaleFactor,
+            landmark[26].x() * imageWidth * scaleFactor,
+            landmark[26].y() * imageHeight * scaleFactor,
+            linePaint
+        )
+
+        canvas.drawLine(
+            landmark[26].x() * imageWidth * scaleFactor,
+            landmark[26].y() * imageHeight * scaleFactor,
+            landmark[24].x() * imageWidth * scaleFactor,
+            landmark[24].y() * imageHeight * scaleFactor,
+            linePaint
+        )
+
+        canvas.drawLine(
+            landmark[24].x() * imageWidth * scaleFactor,
+            landmark[24].y() * imageHeight * scaleFactor,
+            landmark[12].x() * imageWidth * scaleFactor,
+            landmark[12].y() * imageHeight * scaleFactor,
+            linePaint
+        )
+
+        canvas.drawLine(
+            landmark[12].x() * imageWidth * scaleFactor,
+            landmark[12].y() * imageHeight * scaleFactor,
+            landmark[14].x() * imageWidth * scaleFactor,
+            landmark[14].y() * imageHeight * scaleFactor,
+            linePaint
+        )
+
+        canvas.drawLine(
+            landmark[14].x() * imageWidth * scaleFactor,
+            landmark[14].y() * imageHeight * scaleFactor,
+            landmark[16].x() * imageWidth * scaleFactor,
+            landmark[16].y() * imageHeight * scaleFactor,
+            linePaint
+        )
+
+        renderArc(canvas,Pair(landmark[24].x() * imageWidth * scaleFactor, landmark[24].y() * imageHeight * scaleFactor),Pair(landmark[26].x() * imageWidth * scaleFactor, landmark[26].y() * imageHeight * scaleFactor)
+            ,Pair(landmark[28].x() * imageWidth * scaleFactor, landmark[28].y() * imageHeight * scaleFactor))
+
+        renderArc(canvas,Pair(landmark[26].x() * imageWidth * scaleFactor, landmark[26].y() * imageHeight * scaleFactor),Pair(landmark[24].x() * imageWidth * scaleFactor, landmark[24].y() * imageHeight * scaleFactor)
+            ,Pair(landmark[12].x() * imageWidth * scaleFactor, landmark[12].y() * imageHeight * scaleFactor))
+
+        renderArc(canvas,Pair(landmark[24].x() * imageWidth * scaleFactor, landmark[24].y() * imageHeight * scaleFactor),Pair(landmark[12].x() * imageWidth * scaleFactor, landmark[12].y() * imageHeight * scaleFactor)
+            ,Pair(landmark[14].x() * imageWidth * scaleFactor, landmark[14].y() * imageHeight * scaleFactor))
+
+        renderArc(canvas,Pair(landmark[16].x() * imageWidth * scaleFactor, landmark[16].y() * imageHeight * scaleFactor),Pair(landmark[14].x() * imageWidth * scaleFactor, landmark[14].y() * imageHeight * scaleFactor)
+            ,Pair(landmark[12].x() * imageWidth * scaleFactor, landmark[12].y() * imageHeight * scaleFactor))
+    }
+
+    private fun renderFull (canvas:Canvas,landmark: List<NormalizedLandmark>) {
+        renderLeft(canvas, landmark)
+        renderRight(canvas, landmark)
+
+        canvas.drawLine(
+            landmark[23].x() * imageWidth * scaleFactor,
+            landmark[23].y() * imageHeight * scaleFactor,
+            landmark[24].x() * imageWidth * scaleFactor,
+            landmark[24].y() * imageHeight * scaleFactor,
+            linePaint
+        )
+
+        canvas.drawLine(
+            landmark[11].x() * imageWidth * scaleFactor,
+            landmark[11].y() * imageHeight * scaleFactor,
+            landmark[12].x() * imageWidth * scaleFactor,
+            landmark[12].y() * imageHeight * scaleFactor,
+            linePaint
+        )
     }
 
     fun setResults(
@@ -113,6 +304,7 @@ class OverlayView(context: Context?, attrs: AttributeSet?) :
     }
 
     companion object {
-        private const val LANDMARK_STROKE_WIDTH = 12F
+        private const val LANDMARK_STROKE_WIDTH = 6F
+        private const val LANDMARK_POINT_WIDTH = 12F
     }
 }
